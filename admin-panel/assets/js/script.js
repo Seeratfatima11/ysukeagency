7f3d5ccc189bb2cd1a5cf71f12135634eab34f63
SaharTabbasum786 <sahartabbasum013@gmail.com>
2025-04-24 16:21:50 +0500

    Login Form Reset Form Verify Form Create New Password Form In Admin Panel Has Been Done

---
+++ admin-panel/assets/js/script.js
@@ -0,0 +1,11 @@
+// password show/hide js
+$(".toggle-password").click(function () {
+  $(this).toggleClass("fa-eye fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
+// create password show/hide js
